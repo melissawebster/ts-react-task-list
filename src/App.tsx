352a0dfe2916496e39ee9736e1140431d35ef9a7
melissawebster <imelissawebster@gmail.com>
2024-04-20 20:58:19 -0300

    basic Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './App.css'
 import CourseGoal from './components/CourseGoal'
+import goalsImage from './assets/goals.jpg'
+import Header from './components/Header'
 
 function App() {
-
+  console.log(typeof goalsImage)
 
   return (
-    <>
+    <main>
+      <Header 
+        image={{src: goalsImage, alt: "lalala"}}
+      >
+        <h2>Your tasks</h2>
+      </Header>
       <CourseGoal
         title="First task"
         description="Task description"  
       />
-    </>
+    </main>
   )
 }
 
